cabb87099a19fea35a7b1d437b31825d74c58c42
Nicolas Duchoud <n.duchoud@micropool.ch>
2020-08-06 15:50:22 +0200

    old vs modern

--- test/catchRecursion.cpp
+++ test/catchRecursion.cpp
@@ -5,6 +5,7 @@
 #include "helper.h"
 #include <vector>
 #include <bitset>
+#include <array>
 
 void Combi(std::vector<int> input, int reqLen, int currentBit, int currLen)
 // print the all possible combination of given array set
@@ -66,16 +67,110 @@ TEST_CASE("do some recursion") {
 
 
 TEST_CASE("combi from tutorialspoint.com") {
-	
-	
 	std::vector<int> input{5,2,3,4};
 	std::cout << "\n";
 	for ( auto i: {1,2,3} ) {
 		std::cout << "\nThe all possible combination of length " << i << " for the given array set:\n";
-		Combi(input, i, 0, 0);
+		//Combi(input, i, 0, 0);
 	}
 	for (auto i : { 3,2,1 }) {
 		std::cout << "\nThe all possible combination of length " << i << " for the given array set:\n";
-		Combi(input, i, 0, 0);
+		//Combi(input, i, 0, 0);
 	}
+}
+
+const int spectrumSize = 3;
+std::array<long, spectrumSize> spec{ LONG_MAX, LONG_MAX, 000 };
+
+// Generates a comma sperated string of the spectrum data
+std::string toString(const std::array<long, spectrumSize>& spec, const double iRepFactor) {
+	// Convert to a string
+	// C-String of maximal size
+	static char szString[ (10+1) * spectrumSize];
+	// One item in c representation
+	static char szDigits[10];
+	int uiStringArrayPos = 0;
+	int uiDigitArrayPos = 0;
+	unsigned int uiValue = 0;
+	char chrLocal = 0;
+
+	for (int i = 0; i < spectrumSize; ++i) {
+
+		uiDigitArrayPos = 0;
+
+		double tmp = 0.0;
+		if (spec[i] > 0) {
+			tmp = spec[i];
+		}
+		uiValue = static_cast<unsigned int> (floor(tmp * iRepFactor + 0.5));
+		// Enforce limit
+		uiValue = uiValue < 0x7FFFFFFF ? uiValue : 0x7FFFFFFF;
+
+		// Get and save least significant digit
+		while (uiValue > 0) {
+			szDigits[uiDigitArrayPos++] = uiValue % 10;
+			uiValue /= 10;
+		}
+
+		// Build c-style string
+		for (int i = uiDigitArrayPos - 1; i >= 0; i--) {
+			chrLocal = szDigits[i];
+			if (chrLocal >= 0 && chrLocal <= 9) {
+				szString[uiStringArrayPos++] = chrLocal + '0';
+			}
+			//else {
+			//  // Skip digit
+			//  szString[uiStringArrayPos++] = '@';
+			//}
+		}
+
+		// Special case zero
+		if (uiDigitArrayPos == 0) {
+			szString[uiStringArrayPos++] = '0';
+		}
+
+		// Separator
+		szString[uiStringArrayPos++] = ',';
+	}
+
+	// Replace last separator with termination symbol
+	szString[uiStringArrayPos - 1] = 0;
+
+	std::string strResult(szString);
+	return strResult;
+}
+
+
+
+TEST_CASE("spectrum to string before") {
+	std::cout << "spectrum to string before" << std::endl;
+
+	std::cout << toString(spec,1) << std::endl;
+}
+
+
+// Generates a comma sperated string of the spectrum data
+std::string toStringClean(const std::array<long, spectrumSize>& spec, const double iRepFactor) {
+	std::string returnString{ "" };
+
+	for (auto val : spec) {
+		unsigned int uiValue = 0;
+		if (val > 0) {
+			uiValue = static_cast<unsigned int> (floor(val * iRepFactor + 0.5));
+		}
+		else {
+			uiValue = 0;
+		}
+		returnString += std::to_string(uiValue) + ','; // add value plus comma
+	}
+	returnString.pop_back(); // remove the last comma ( 1,2,3, => 1,2,3 )
+	return returnString;
+}
+
+
+
+TEST_CASE("spectrum to string after") {
+	std::cout << "spectrum to string after" << std::endl;
+	std::cout << toStringClean(spec, 1) << std::endl;
+	REQUIRE( toStringClean(spec, 1) == toString(spec, 1) );
 }
